f38c7e27754048cfcc4d660fdc5103429b821d29
Nikko <nikoaguiar900@gmail.com>
2023-05-11 17:22:50 -0300

    Update

--- validacao_de_numeros/main.js
+++ validacao_de_numeros/main.js
@@ -1,3 +1,7 @@
+const containerMensagemSucesso = document.querySelector('.mensagem_sucesso');
+const containerMensagemErro = document.querySelector('.mensagem_erro');
+const containerMensagemIgual = document.querySelector('.mensagem_igual');
+
 const form = document.getElementById('form_dep');
 
 form.addEventListener('submit', function(e){
@@ -5,17 +9,33 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
     const numA = document.getElementById('campo_A');
     const numB = document.getElementById('campo_B');
+    const mensagemSucesso = `O valor ${numB.value} é maior que o valor ${numA.value}.`;
+    const mensagemErro = `O valor ${numB.value} não é maior que o valor ${numA.value}.`;
+    const mensagemIgual = `O valor ${numB.value} e o valor ${numA.value} são iguais!!`
     if(parseInt(numA.value) < parseInt(numB.value)){
         // console.log('B é maior que A!')
-        console.log(numB.value)
-        alert("B é maior que A!!");
+        // console.log(numB.value)
+        // alert("B é maior que A!!");
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+        containerMensagemErro.style.display = 'none';
+        containerMensagemIgual.style.display = 'none';
+        numA.value = '';
+        numB.value = '';
+    }else if(parseInt(numA.value) > parseInt(numB.value)){
+        // console.log('A é maior!')
+        // console.log(numA.value)
+        // alert('B não é maior que A!!');
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+        containerMensagemSucesso.style.display = 'none';
+        containerMensagemIgual.style.display = 'none';
         numA.value = '';
         numB.value = '';
     }else{
-        // console.log('A é maior!')
-        console.log(numA.value)
-        alert('B não é maior que A!!');
-        numA.value = '';
-        numB.value = '';
+        containerMensagemIgual.innerHTML = mensagemIgual;
+        containerMensagemIgual.style.display = 'block';
+        containerMensagemErro.style.display = 'none';
+        containerMensagemSucesso.style.display = 'none';
     }
 })
