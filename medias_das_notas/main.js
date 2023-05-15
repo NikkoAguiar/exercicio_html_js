94f24f3953d36b1507eb98ea73c839ccb2518c84
Nikko <nikoaguiar900@gmail.com>
2023-05-15 16:53:54 -0300

    Update

--- medias_das_notas/main.js
+++ medias_das_notas/main.js
@@ -1,7 +1,11 @@
 const form = document.getElementById('form_dep');
 const containerMensagemErro = document.querySelector('.mensagem_erro');
 const containerMensagemErroNome = document.querySelector('.mensagem_erro2');
+const containerlabel = document.querySelector('.label1');
 const nome = document.getElementById('campo_1');
+const atividade = document.getElementById('atividade1');
+const nota = document.getElementById('nota1');
+const aprovado = document.getElementById('aprovado1');
 let formValido = false;
 function validaNome(nomecorreto){
     const nomeComoArray = nomecorreto.split(' ');
@@ -15,6 +19,9 @@ form.addEventListener('submit', function(e){
     const nota3 = document.getElementById('campo_4');
     const mensagemErroNome = `Precisa ter nome e sobrenome!`;
     const mensagemErro = `Não são permitidos números negativos!`;
+    const nomeAluno = `Nome do aluno: ${nome}`;
+    console.log(nome.value);
+    
 
     formValido = validaNome(nome.value);
 
@@ -22,13 +29,17 @@ form.addEventListener('submit', function(e){
         containerMensagemErroNome.innerHTML = mensagemErroNome;
         containerMensagemErroNome.style.display = 'block';
     }else{
+        containerlabel.innerHTML = nomeAluno;
+        containerlabel.style.display = 'block';
         containerMensagemErroNome.style.display = 'none';
         if(parseFloat(nota1.value) < 0 || parseFloat(nota2.value) < 0 || parseFloat(nota3.value) < 0){
             containerMensagemErro.innerHTML = mensagemErro;
             containerMensagemErro.style.display = 'block';
         }else{
+            
             containerMensagemErro.style.display = 'none';
             let res = parseFloat(nota1.value) + parseFloat(nota2.value) + parseFloat(nota3.value) / 3;
+
         }
     }
 })
